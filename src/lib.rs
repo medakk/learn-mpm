0bf90e0c13a424e793634dfcdabd393bfe75e5ef
Karthik Karanth <karanth.karthik@gmail.com>
2021-01-05 22:44:55 -0700

    implement part 3 sorta wonky

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ extern crate rand;
 extern crate nalgebra_glm as glm;
 
 const CELL_DIM: usize = 64;
-const GRAVITY: f32 = 0.3;
-const ELASTIC_LAMBDA: f32 = 10.0;
-const ELASTIC_MU: f32 = 20.0;
+const GRAVITY: f32 = 0.4;
+const REST_DENSITY: f32 = 9.0;
+const DYNAMIC_VISCOSITY: f32 = 0.11;
+const EOS_STIFFNESS: f32 = 13.1;
+const EOS_POWER: f32 = 7.0;
 
 fn to_uint_vec(v: &glm::Vec2) -> glm::UVec2 {
     return glm::vec2(v.x as u32, v.y as u32);
@@ -21,7 +23,6 @@ pub struct Particle {
     pub v: glm::Vec2,
     pub C: glm::Mat2,
     pub F: glm::Mat2,
-    pub volume_0: f32,
     pub mass: f32,
 }
 
@@ -95,7 +96,6 @@ impl MPM {
                     v: 2.0*glm::vec2(rand::random::<f32>()-0.5, rand::random::<f32>()-0.5),
                     C: glm::zero(),
                     F: glm::mat2(1.0, 0.0, 0.0, 1.0),
-                    volume_0: 0.0,
                     mass: 1.0,
                 };
                 particles.push(p);
@@ -104,47 +104,15 @@ impl MPM {
 
         self.cells = cells;
         self.particles = particles;
-
-        self.compute_volume_0();
     }
 
     pub fn update(&mut self, dt: f32) {
         self.reset_cells();
 
-        self.p2g(dt);
+        self.p2g_1(dt);
+        self.p2g_2(dt);
         self.cells_update(dt);
         self.g2p(dt);
-    }
-
-    fn compute_volume_0(&mut self) {
-        self.p2g(0.0);
-
-        for p in self.particles.iter_mut() {
-            let cell_idx: glm::UVec2 = to_uint_vec(&p.x);
-            let cell_diff: glm::Vec2 = (&p.x - to_f32_vec(&cell_idx)) - glm::vec2(0.5, 0.5);
-
-            let weights = [
-                0.5 * glm::pow(&(glm::vec2(0.5, 0.5) - &cell_diff), &glm::vec2(2.0, 2.0)),
-                glm::vec2(0.75, 0.75) - glm::pow(&cell_diff, &glm::vec2(2.0, 2.0)),
-                0.5 * glm::pow(&(glm::vec2(0.5, 0.5) + &cell_diff), &glm::vec2(2.0, 2.0)),
-            ];
-
-            let mut density = 0.0;
-            for gx in 0..3 {
-                for gy in 0..3 {
-                    let weight = weights[gx].x * weights[gy].y;
-                    let cell_x = glm::vec2(cell_idx.x + gx as u32 - 1,
-                                           cell_idx.y + gy as u32 - 1);
-                    let cell_idx = cell_x.x as usize * CELL_DIM + cell_x.y as usize;
-                    if cell_idx >= self.cells.len() {
-                        continue;
-                    }
-                    density += self.cells[cell_idx].mass * weight;
-                }
-            }
-
-            p.volume_0 = p.mass / density;
-        }
     }
 
     fn reset_cells(&mut self) {
@@ -153,26 +121,8 @@ impl MPM {
         }
     }
 
-    fn p2g(&mut self, dt: f32) {
+    fn p2g_1(&mut self, _dt: f32) {
         for p in self.particles.iter_mut() {
-            let F = &p.F;
-            let J = glm::determinant(F);
-            let volume = p.volume_0 * J;
-
-            // convenience
-            let F_T = glm::transpose(F);
-            let F_inv_T = glm::inverse(&F_T);
-            let F_minus_F_inv_T = F - &F_inv_T;
-
-            // mpm equations
-            let P_term_0 = ELASTIC_MU * &F_minus_F_inv_T;
-            let P_term_1 = ELASTIC_LAMBDA * J.ln() * &F_inv_T;
-            let P = P_term_0 + P_term_1;
-
-            let stress = (1.0 / J) * &P * &F_T;
-
-            let eq_16_term_0 = -volume * 4.0 * stress * dt;
-
             let cell_idx: glm::UVec2 = to_uint_vec(&p.x);
             let cell_diff: glm::Vec2 = (&p.x - to_f32_vec(&cell_idx)) - glm::vec2(0.5, 0.5);
 
@@ -181,6 +131,81 @@ impl MPM {
                 glm::vec2(0.75, 0.75) - glm::pow(&cell_diff, &glm::vec2(2.0, 2.0)),
                 0.5 * glm::pow(&(glm::vec2(0.5, 0.5) + &cell_diff), &glm::vec2(2.0, 2.0)),
             ];
+
+            for gx in 0..3 {
+                for gy in 0..3 {
+                    let weight = weights[gx].x * weights[gy].y;
+                    let cell_x = glm::vec2(cell_idx.x + gx as u32 -1,
+                                           cell_idx.y + gy as u32 - 1);
+                    let cell_dist = (to_f32_vec(&cell_x) - &p.x) + glm::vec2(0.5, 0.5);
+                    let Q = &p.C * &cell_dist;
+
+                    let mass_contrib = weight * p.mass;
+
+                    let cell_idx = cell_x.x as usize * CELL_DIM + cell_x.y as usize;
+                    if cell_idx >= self.cells.len() {
+                        continue;
+                    }
+
+                    let cell = &mut self.cells[cell_idx];
+                    cell.mass += mass_contrib;
+                    cell.v += mass_contrib * (&p.v + Q);
+                }
+            }
+        }
+    }
+
+    fn p2g_2(&mut self, dt: f32) {
+        for p in self.particles.iter_mut() {
+            let cell_idx: glm::UVec2 = to_uint_vec(&p.x);
+            let cell_diff: glm::Vec2 = (&p.x - to_f32_vec(&cell_idx)) - glm::vec2(0.5, 0.5);
+
+            let weights = [
+                0.5 * glm::pow(&(glm::vec2(0.5, 0.5) - &cell_diff), &glm::vec2(2.0, 2.0)),
+                glm::vec2(0.75, 0.75) - glm::pow(&cell_diff, &glm::vec2(2.0, 2.0)),
+                0.5 * glm::pow(&(glm::vec2(0.5, 0.5) + &cell_diff), &glm::vec2(2.0, 2.0)),
+            ];
+
+            let density = {
+                let mut d = 0.0f32;
+                for gx in 0..3 {
+                    for gy in 0..3 {
+                        let weight = weights[gx].x * weights[gy].y;
+                        let cell_x = glm::vec2(cell_idx.x + gx as u32 -1,
+                                               cell_idx.y + gy as u32 - 1);
+                        let cell_idx = cell_x.x as usize * CELL_DIM + cell_x.y as usize;
+                        if cell_idx >= self.cells.len() {
+                            continue;
+                        }
+                        d += self.cells[cell_idx].mass * weight;
+                    }
+                }
+                d
+            };
+
+            let volume = p.mass / density;
+            let pressure = (
+                EOS_STIFFNESS * ((density/REST_DENSITY).powf(EOS_POWER) - 1.0)
+            ).max(-0.1);
+
+
+            let eq_16_term_0 = {
+                let mut stress = glm::mat2(
+                    -pressure, 0.0,
+                    0.0, -pressure,
+                );
+                let dudv = &p.C;
+                let mut strain = dudv.clone();
+                let trace = strain.m11 + strain.m22;
+                strain.m11 = trace;
+                strain.m22 = trace;
+
+                let viscosity_term = DYNAMIC_VISCOSITY * &strain;
+                stress += &viscosity_term;
+
+                -volume * 4.0 * &stress * dt
+            };
+
 
             for gx in 0..3 {
                 for gy in 0..3 {
@@ -241,7 +266,6 @@ impl MPM {
                 0.5 * glm::pow(&(glm::vec2(0.5, 0.5) + &cell_diff), &glm::vec2(2.0, 2.0)),
             ];
 
-            let mut B: glm::Mat2 = glm::zero();
             for gx in 0..3 {
                 for gy in 0..3 {
                     let weight = weights[gx].x * weights[gy].y;
@@ -252,28 +276,14 @@ impl MPM {
                         continue;
                     }
 
-                    let cell_dist = (to_f32_vec(&cell_x) - &p.x) + glm::vec2(0.5, 0.5);
+                    // let cell_dist = (to_f32_vec(&cell_x) - &p.x) + glm::vec2(0.5, 0.5);
                     let weighted_v = &self.cells[cell_idx].v * weight;
-
-                    let wx = &weighted_v * cell_dist.x;
-                    let wy = &weighted_v * cell_dist.y;
-
-                    // TODO Transpose?
-                    let term = glm::mat2x2(wx.x, wy.x, wx.y, wy.y);
-                    B += term;
-
                     p.v += weighted_v;
                 }
             }
 
-            p.C = B * 4.0;
-
             p.x += &p.v * dt;
             p.x = glm::clamp(&p.x, 1.0, CELL_DIM as f32-2.0);
-
-            let mut F_new = glm::mat2(1.0, 0.0, 0.0, 1.0);
-            F_new += dt * &p.C;
-            p.F = &F_new * &p.F;
         }
     }
 
